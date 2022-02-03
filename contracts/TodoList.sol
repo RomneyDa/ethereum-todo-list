724c722b45d7703e219710eacc6346912b5745fc
Dallin Romney <dallinromney@gmail.com>
2022-02-03 01:27:25 -0700

    intial addition of client

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -6,5 +6,24 @@ contract TodoList {
     // State variable keeping track of task count
     uint public taskCount = 0;
 
-    
+    // Create data structure for individual tasks
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // Create place for tasks to be stored
+    mapping(uint => Task) public tasks;
+
+    // Function for adding a new task
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+    // Populate with some tasks upon deployment
+    constructor() {
+        createTask("Do this heinous act first!");
+    }
 }
